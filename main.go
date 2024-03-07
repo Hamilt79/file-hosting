46587c5bbc4902506e57c2a7f0559db3154e0aa1
hamilt79 <hamilt79@miamioh.edu>
2024-03-07 11:49:23 -0500

    Added an index page

--- main.go
+++ main.go
@@ -4,9 +4,22 @@ import (
     "fmt"
     "log"
     "net/http"
+    "os"
 )
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+    b, err := os.ReadFile("./public/index.html")
+    if err != nil {
+        return
+    }
+    fmt.Fprintf(w, string(b))
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path == "/" {
+        homeHandler(w, r)
+        return
+    }
     fmt.Fprintf(w, "I love %v", r.URL.Path)
 }
 
